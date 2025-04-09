56c66955b03e176a258209f0dec24833186fe55f
uemerson199 <uemersonribeiro220@gmail.com>
2025-04-09 17:00:04 -0300

    Implementando a persistência com Jakarta Persistence e Spring Data JPA

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -4,19 +4,25 @@ package com.algaworks.algasensors.device.management.api.controller;
 import com.algaworks.algasensors.device.management.api.model.SensorInput;
 import com.algaworks.algasensors.device.management.common.IdGenerator;
 import com.algaworks.algasensors.device.management.domain.model.Sensor;
+import com.algaworks.algasensors.device.management.domain.model.SensorId;
+import com.algaworks.algasensors.device.management.domain.repository.SensorRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/sensors")
+@RequiredArgsConstructor
 public class SensorController {
 
+
+    private final SensorRepository sensorRepository;
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     public Sensor create (@RequestBody SensorInput input) {
-        return Sensor.builder()
-                .id(IdGenerator.generateTSID())
+        Sensor sensor = Sensor.builder()
+                .id(new SensorId(IdGenerator.generateTSID()))
                 .name(input.getName())
                 .ip(input.getIp())
                 .location(input.getLocation())
@@ -24,6 +30,8 @@ public class SensorController {
                 .model(input.getModel())
                 .enabled(false)
                 .build();
+
+        return sensorRepository.saveAndFlush(sensor);
     }
 
 
