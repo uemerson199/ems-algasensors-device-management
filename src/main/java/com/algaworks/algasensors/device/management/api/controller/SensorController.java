642551bd6cec07ef4bc0e87cd5c0e7b9f95eb349
uemerson199 <uemersonribeiro220@gmail.com>
2025-04-11 16:07:03 -0300

    Desafio: inativação dos sensores

--- src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/controller/SensorController.java
@@ -75,12 +75,33 @@ public class SensorController {
     }
 
     @DeleteMapping("{sensorId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable TSID sensorId) {
         Sensor sensor = sensorRepository.findById(new SensorId(sensorId)).
                 orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
 
         sensorRepository.delete(sensor);
     }
+
+    @PutMapping("{sensorId}/enable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void enable (@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId)).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        sensor.setEnabled(true);
+        sensorRepository.save(sensor);
+    }
+
+    @PutMapping("{sensorId}/disable")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void disable (@PathVariable TSID sensorId) {
+        Sensor sensor = sensorRepository.findById(new SensorId(sensorId)).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        sensor.setEnabled(false);
+        sensorRepository.save(sensor);
+    }
+
+
 
     private SensorOutput convertToModel(Sensor sensor) {
         return SensorOutput.builder()
