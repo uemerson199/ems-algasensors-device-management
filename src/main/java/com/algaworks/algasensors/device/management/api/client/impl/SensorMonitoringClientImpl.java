f6cd396d33b82163c77fa1f9297b1eaf164158e2
uemerson199 <uemersonribeiro220@gmail.com>
2025-04-16 11:29:14 -0300

    Simplificando a implementação com Factory Pattern

--- src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,38 +1,18 @@
 package com.algaworks.algasensors.device.management.api.client.impl;
 
+import com.algaworks.algasensors.device.management.api.client.RestClientFactory;
 import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClient;
-import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
-
-import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
     private final RestClient restClient;
 
-    public SensorMonitoringClientImpl(RestClient.Builder builder ) {
-        this.restClient = builder.baseUrl("http://localhost:8082")
-                .requestFactory(generateClientHttpRequestFactory())
-                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
-                    throw new SensorMonitoringClientBadGatewayException();
-                })
-                .build();
-    }
-
-    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
-        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-
-        factory.setReadTimeout(Duration.ofSeconds(5));
-        factory.setConnectTimeout(Duration.ofSeconds(3));
-
-        return factory;
-
+    public SensorMonitoringClientImpl(RestClientFactory factory) {
+        this.restClient = factory.temperatureMonitoringRestClient();
     }
 
     @Override
