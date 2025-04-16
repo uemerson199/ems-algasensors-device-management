1aebc0b05bc42d1d10557cb971d635d92c42118b
uemerson199 <uemersonribeiro220@gmail.com>
2025-04-15 21:06:46 -0300

    HTTP Status Codes para erros na integração

---
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/SensorMonitoringClientBadGatewayException.java
@@ -0,0 +1,9 @@
+package com.algaworks.algasensors.device.management.api.client;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_GATEWAY)
+public class SensorMonitoringClientBadGatewayException extends RuntimeException {
+
+}
--- src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -1,7 +1,9 @@
 package com.algaworks.algasensors.device.management.api.client.impl;
 
 import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClient;
+import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
 
@@ -11,7 +13,11 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
     private final RestClient restClient;
 
     public SensorMonitoringClientImpl(RestClient.Builder builder ) {
-        this.restClient = builder.baseUrl("http://localhost:8082").build();
+        this.restClient = builder.baseUrl("http://localhost:8082")
+                .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
+                    throw new SensorMonitoringClientBadGatewayException();
+                })
+                .build();
     }
 
     @Override
