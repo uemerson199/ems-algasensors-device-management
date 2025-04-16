d8080742cc4c2abf70ad6b13c671ab78217027cf
uemerson199 <uemersonribeiro220@gmail.com>
2025-04-16 11:12:59 -0300

    Lidando com erros de timeout

--- src/main/java/com/algaworks/algasensors/device/management/api/client/SensorMonitoringClientBadGatewayException.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/SensorMonitoringClientBadGatewayException.java
@@ -1,9 +1,6 @@
 package com.algaworks.algasensors.device.management.api.client;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(HttpStatus.BAD_GATEWAY)
 public class SensorMonitoringClientBadGatewayException extends RuntimeException {
 
 }
--- src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
+++ src/main/java/com/algaworks/algasensors/device/management/api/client/impl/SensorMonitoringClientImpl.java
@@ -4,8 +4,12 @@ import com.algaworks.algasensors.device.management.api.client.SensorMonitoringCl
 import com.algaworks.algasensors.device.management.api.client.SensorMonitoringClientBadGatewayException;
 import io.hypersistence.tsid.TSID;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClient;
+
+import java.time.Duration;
 
 @Component
 public class SensorMonitoringClientImpl implements SensorMonitoringClient {
@@ -14,10 +18,21 @@ public class SensorMonitoringClientImpl implements SensorMonitoringClient {
 
     public SensorMonitoringClientImpl(RestClient.Builder builder ) {
         this.restClient = builder.baseUrl("http://localhost:8082")
+                .requestFactory(generateClientHttpRequestFactory())
                 .defaultStatusHandler(HttpStatusCode::isError, (request, response) -> {
                     throw new SensorMonitoringClientBadGatewayException();
                 })
                 .build();
+    }
+
+    private ClientHttpRequestFactory generateClientHttpRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+
+        factory.setReadTimeout(Duration.ofSeconds(5));
+        factory.setConnectTimeout(Duration.ofSeconds(3));
+
+        return factory;
+
     }
 
     @Override
